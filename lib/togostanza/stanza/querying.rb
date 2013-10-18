603a9f6a081709a7ee8402dc88e699ccdb36c50f
Keita Urashima <ursm@ursm.jp>
2013-10-18 21:25:34 +0900

    Sync endpoint related features with master

--- lib/togostanza/stanza/querying.rb
+++ lib/togostanza/stanza/querying.rb
@@ -3,9 +3,9 @@ require 'sparql/client'
 module TogoStanza::Stanza
   module Querying
     MAPPINGS = {
-      togogenome: 'http://ep.dbcls.jp/sparql7os',
-      uniprot:    'http://ep.dbcls.jp/sparql7os',
-      go:         'http://ep.dbcls.jp/sparql7os'
+      togogenome: 'http://ep.dbcls.jp/sparql7',
+      uniprot:    'http://ep.dbcls.jp/sparql7',
+      go:         'http://ep.dbcls.jp/sparql7'
     }
 
     def query(endpoint, sparql)
@@ -23,26 +23,20 @@ module TogoStanza::Stanza
     def uniprot_url_from_togogenome(gene_id)
       # refseq の UniProt
       # slr1311 の時 "http://purl.uniprot.org/refseq/NP_439906.1"
-      query(:togogenome, <<-SPARQL.strip_heredoc).first[:up]
-        PREFIX insdc: <http://insdc.org/owl/>
-        PREFIX idorg: <http://rdf.identifiers.org/database/>
-        PREFIX dct:   <http://purl.org/dc/terms/>
+      query(:togogenome, <<-SPARQL).first[:up]
+      PREFIX insdc: <http://insdc.org/owl/>
+      PREFIX idorg: <http://rdf.identifiers.org/database/>
 
-        SELECT DISTINCT ?up
-        WHERE {
-          GRAPH <http://togogenome.org/graph/> {
-            <http://togogenome.org/refseq/> dct:isVersionOf ?g .
-          }
-
-          GRAPH ?g {
-            ?s insdc:feature_locus_tag "#{gene_id}" .
-            ?s rdfs:seeAlso ?np .
-            ?s rdfs:seeAlso ?xref .
-            ?np rdf:type idorg:Protein .
-            BIND (STRAFTER(STR(?np), "ncbiprotein/") AS ?npid)
-            BIND (IRI(CONCAT("http://purl.uniprot.org/refseq/", ?npid)) AS ?up)
-          }
-        }
+      SELECT DISTINCT ?up
+      FROM <http://togogenome.org/graph/refseq/>
+      WHERE {
+          ?s insdc:feature_locus_tag "#{gene_id}" .
+          ?s rdfs:seeAlso ?np .
+          ?s rdfs:seeAlso ?xref .
+          ?np rdf:type idorg:Protein .
+          BIND (STRAFTER(STR(?np), "ncbiprotein/") AS ?npid)
+          BIND (IRI(CONCAT("http://purl.uniprot.org/refseq/", ?npid)) AS ?up)
+      }
       SPARQL
     end
   end
