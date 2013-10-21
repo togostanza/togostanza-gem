8b837e45c65bfc93fc41b8c915355df3459287fc
Keita Urashima <ursm@ursm.jp>
2013-10-22 05:39:36 +0900

    Implement generators

--- lib/togostanza/cli.rb
+++ lib/togostanza/cli.rb
@@ -1,31 +1,70 @@
 require 'thor'
-require 'togostanza'
+require 'active_support/all'
 
 module TogoStanza
   module CLI
-    class NewStanza < Thor::Group
+    class ProviderGenerator < Thor::Group
       include Thor::Actions
 
       argument :name
 
-      def foo
-        puts name
+      def self.source_root
+        File.expand_path('../../../templates/provider', __FILE__)
+      end
+
+      def create_files
+        template 'Gemfile.erb',   "#{name}/Gemfile"
+        template 'config.ru.erb', "#{name}/config.ru"
+      end
+    end
+
+    class StanzaGenerator < Thor::Group
+      include Thor::Actions
+
+      argument :name
+
+      def self.source_root
+        File.expand_path('../../../templates/stanza', __FILE__)
+      end
+
+      def create_files
+        template 'Gemfile.erb',      "#{file_name}/Gemfile"
+        template 'gemspec.erb',      "#{file_name}/#{file_name}.gemspec"
+        template 'lib.rb.erb',       "#{file_name}/lib/#{file_name}.rb"
+        template 'stanza.rb.erb',    "#{file_name}/stanza.rb"
+        template 'template.hbs.erb', "#{file_name}/template.hbs"
+        template 'help.md.erb',      "#{file_name}/help.md"
+      end
+
+      def inject_gem
+        append_to_file 'Gemfile', "gem '#{file_name}', path: './#{file_name}'"
+      end
+
+      private
+
+      def stanza_id
+        name.underscore.sub(/_stanza$/, '')
+      end
+
+      def file_name
+        stanza_id + '_stanza'
+      end
+
+      def class_name
+        file_name.classify
+      end
+
+      def title
+        stanza_id.titleize
       end
     end
 
     class Stanza < Thor
-      register NewStanza, 'new', 'new NAME', 'Generate a new stanza'
+      register StanzaGenerator, 'new', 'new NAME', 'Creates a new stanza'
     end
 
     class Root < Thor
-      desc 'init [NAME]', 'Initialize stanza provider'
-      def init
-      end
-
-      desc 'server', 'Launch app server'
-      def server
-        TogoStanza::Application.run!
-      end
+      register ProviderGenerator, 'init', 'init NAME', 'Creates a new provider'
 
       desc 'stanza [COMMAND]', ''
       subcommand 'stanza', Stanza
