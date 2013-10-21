1e2da8edcd2030913e26427fe882b47224a73beb
Keita Urashima <ursm@ursm.jp>
2013-10-21 19:25:04 +0900

    WIP: Add generator skelton

--- lib/togostanza/cli.rb
+++ lib/togostanza/cli.rb
@@ -3,11 +3,32 @@ require 'togostanza'
 
 module TogoStanza
   module CLI
+    class NewStanza < Thor::Group
+      include Thor::Actions
+
+      argument :name
+
+      def foo
+        puts name
+      end
+    end
+
+    class Stanza < Thor
+      register NewStanza, 'new', 'new NAME', 'Generate a new stanza'
+    end
+
     class Root < Thor
+      desc 'init [NAME]', 'Initialize stanza provider'
+      def init
+      end
+
       desc 'server', 'Launch app server'
       def server
         TogoStanza::Application.run!
       end
+
+      desc 'stanza [COMMAND]', ''
+      subcommand 'stanza', Stanza
     end
   end
 end
