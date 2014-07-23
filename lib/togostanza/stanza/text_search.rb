32b08fc89c2ca64c9e02673a5bc2a94c5d72b8e6
takashi hatakeyama <takashi.hatakeyama@gmail.com>
2014-07-23 15:46:43 +0900

    search 宣言でテキスト検索を行えるようにした
    property と対称を持つよう、以下のような search 宣言でテキスト検索を行うようにした。
    
    ```
    search :organism_name_list do |q|
      ...
    end
    ```

--- lib/togostanza/stanza/text_search.rb
+++ lib/togostanza/stanza/text_search.rb
@@ -1,7 +1,25 @@
+require 'active_support/core_ext/class/attribute'
+
 module TogoStanza::Stanza
   module TextSearch
     def text_search(q)
-      raise NotImplementedError
+      search_declarations.each_value.flat_map {|block| instance_exec(q, &block) }
+    end
+
+    module ClassMethods
+      def search(symbol, &block)
+        search_declarations[symbol] = block
+      end
+    end
+
+    def self.included(klass)
+      klass.extend ClassMethods
+
+      klass.instance_eval do
+        # テキスト検索宣言の初期化
+        class_attribute :search_declarations
+        self.search_declarations = {}
+      end
     end
   end
 end
