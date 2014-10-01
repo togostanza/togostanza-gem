5fb246c2c2f45694fefdd0c931c2f1563c9fc043
Naoki Nishiguchi <nnishiguchister@gmail.com>
2014-10-01 15:54:15 +0900

    Fix scroll

--- lib/togostanza/stanza/base.rb
+++ lib/togostanza/stanza/base.rb
@@ -5,7 +5,7 @@ require 'hashie/mash'
 FS.register_helper :adjust_iframe_height_script do
   <<-HTML.strip_heredoc.html_safe
     <script>$(function() {
-      height = this.body.offsetHeight + 30;
+      height = this.body.offsetHeight + 43;
       parent.postMessage(JSON.stringify({height: height, id: name}), "*");
     });</script>
   HTML
