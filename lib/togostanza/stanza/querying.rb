201182f358d27ce6ae21ee0c05748c89c75ca998
Naoki Nishiguchi <nnishiguchister@gmail.com>
2015-11-17 15:26:44 +0900

    Be able to use the sparql path in the second argument

--- lib/togostanza/stanza/querying.rb
+++ lib/togostanza/stanza/querying.rb
@@ -1,4 +1,5 @@
 require 'sparql/client'
+require 'flavour_saver'
 
 module TogoStanza::Stanza
   module Querying
@@ -6,12 +7,19 @@ module TogoStanza::Stanza
       togogenome: 'http://togogenome.org/sparql'
     }
 
-    def query(endpoint, sparql)
+    def query(endpoint, text_or_filename)
+      path = File.join(root, 'sparql', text_or_filename)
+
+      if File.exist?(path)
+        data = OpenStruct.new params
+        text_or_filename = Tilt.new(path).render(data)
+      end
+
       client = SPARQL::Client.new(MAPPINGS[endpoint] || endpoint)
 
       #Rails.logger.debug "SPARQL QUERY: \n#{sparql}"
 
-      client.query(sparql).map {|binding|
+      client.query(text_or_filename).map {|binding|
         binding.each_with_object({}) {|(name, term), hash|
           hash[name] = term.to_s
         }
